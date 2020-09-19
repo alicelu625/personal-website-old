c16aa4e771ef8d996a8413d19e5df11dc1980558
Alice <alicelu625@gmail.com>
2020-09-18 19:03:05 -0700

    Updates

---
+++ precache-manifest.86cfea206e98cc117c3d80fa2dd88346.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f01805dd36986c96bb2702854be91ddc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28875f0a86249fb56b50",
+    "url": "/static/css/main.c39d7411.chunk.css"
+  },
+  {
+    "revision": "d7df67cc94b60d6d1c98",
+    "url": "/static/js/2.8f6184fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8f6184fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28875f0a86249fb56b50",
+    "url": "/static/js/main.50be55a2.chunk.js"
+  },
+  {
+    "revision": "f139b624154a37532bb8",
+    "url": "/static/js/runtime-main.ce89729d.js"
+  },
+  {
+    "revision": "361ac4b8e23f63ae0e625499542ff101",
+    "url": "/static/media/AliceLuResume.361ac4b8.pdf"
+  },
+  {
+    "revision": "c2b45ce82fb9c39e5ed6b805dfd38560",
+    "url": "/static/media/baskin-logo.c2b45ce8.jpg"
+  },
+  {
+    "revision": "bae02e15f13931c5c44b2e571f7123ec",
+    "url": "/static/media/burger-builder-logo.bae02e15.png"
+  },
+  {
+    "revision": "9d83dad464a59890604728b90a3830b9",
+    "url": "/static/media/csa-logo.9d83dad4.jpg"
+  },
+  {
+    "revision": "f2cd96feb96df4479cb2061834bfe46b",
+    "url": "/static/media/digital-green-logo.f2cd96fe.jpg"
+  },
+  {
+    "revision": "600647e92a44583213e3b04e21a1b94f",
+    "url": "/static/media/gateway-learning-group-logo.600647e9.jpg"
+  },
+  {
+    "revision": "da2bbabd0722a1e8bb6ad4caa08cf6e7",
+    "url": "/static/media/profile-picture.da2bbabd.jpeg"
+  },
+  {
+    "revision": "71d588c658ae173ae1972ae0300b62bb",
+    "url": "/static/media/ucsc-logo.71d588c6.png"
+  }
+]);
